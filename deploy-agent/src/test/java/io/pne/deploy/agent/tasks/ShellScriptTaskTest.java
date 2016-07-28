b304fa714a4b8ce07d16f53d59e6feea3040a815
Evgeniy Sinev <es@payneteasy.com>
2016-07-28 03:45:12 +0300

    Fixed ShellScriptTaskTest test

--- deploy-agent/src/test/java/io/pne/deploy/agent/tasks/ShellScriptTaskTest.java
+++ deploy-agent/src/test/java/io/pne/deploy/agent/tasks/ShellScriptTaskTest.java
@@ -7,21 +7,21 @@ import io.pne.deploy.api.tasks.ShellScriptParameters;
 import io.pne.deploy.api.tasks.ShellScriptResult;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.InOrder;
-
-import static org.mockito.Mockito.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class ShellScriptTaskTest {
 
+
     @Test
-    public void test_sh() {
+    public void test_sh() throws InterruptedException {
 
         ShellScriptTask task = new ShellScriptTask(new File("./src/test/resources/scripts"));
-
-        ITaskContext context = mock(ITaskContext.class);
 
         ImmutableShellScriptParameters parameters = ImmutableShellScriptParameters.builder()
                 .taskId(UUID.randomUUID().toString())
@@ -31,14 +31,35 @@ public class ShellScriptTaskTest {
                 .environment(ImmutableMap.of("ENV_VARIABLE", "env-value"))
                 .build();
 
+        TestContext context = new TestContext();
         ShellScriptResult result = task.execute(context, parameters);
         Assert.assertEquals(0, result.exitCode());
 
-        InOrder inOrder = inOrder(context);
-        inOrder.verify(context).log("ARGUMENT_1=test.sh");
-        inOrder.verify(context).log("ENV_VARIABLE=env-value");
-        inOrder.verify(context).log("ERROR OUTPUT");
-        inOrder.verify(context).log("STD OUTPUT");
+        Assert.assertTrue("ITaskContext.log() must be invoked 4 times", context.logsLatch.await(2, TimeUnit.SECONDS));
 
+        Assert.assertEquals("ARGUMENT_1=test.sh"    , context.logs.get(0));
+        Assert.assertEquals("ENV_VARIABLE=env-value", context.logs.get(1));
+        Assert.assertEquals("ERROR OUTPUT"          , context.logs.get(2));
+        Assert.assertEquals("STD OUTPUT"            , context.logs.get(3));
+
+        Assert.assertNull(context.result);
+
+    }
+
+    public static class TestContext implements ITaskContext {
+        List<String> logs = new ArrayList<>();
+        ShellScriptResult result;
+        CountDownLatch logsLatch = new CountDownLatch(4);
+
+        @Override
+        public void log(String aLine) {
+            logs.add(aLine);
+            logsLatch.countDown();
+        }
+
+        @Override
+        public void sendResultToServer(ShellScriptResult aResult) {
+            result = aResult;
+        }
     }
 }
