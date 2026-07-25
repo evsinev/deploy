a37236e0da63d6bed7b8675051bccc452d9203f9
esinev <es@payneteasy.com>
2026-07-26 00:24:14 +0300

    fix(redmine): null-safe issue mapping for missing custom fields and assignee

--- client-redmine/src/test/java/io/pne/deploy/client/redmine/remote/impl/RemoteRedmine4_2_10ServiceImplTest.java
+++ client-redmine/src/test/java/io/pne/deploy/client/redmine/remote/impl/RemoteRedmine4_2_10ServiceImplTest.java
@@ -2,10 +2,29 @@ package io.pne.deploy.client.redmine.remote.impl;
 
 import com.payneteasy.startup.parameters.StartupParametersFactory;
 import io.pne.deploy.client.redmine.remote.IRemoteRedmineService;
+import io.pne.deploy.client.redmine.remote.data_model.RedmineIssueData;
+import io.pne.deploy.client.redmine.remote.model.RedmineIssue;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class RemoteRedmine4_2_10ServiceImplTest {
+
+    @Test
+    public void mapIssueToleratesMissingCustomFieldsAndAssignee() {
+        // Redmine omits custom_fields / assigned_to for issues that have none; must not NPE.
+        RedmineIssueData data = RedmineIssueData.builder()
+                .id(167994)
+                .subject("Referenced issue")
+                .build();
+        RedmineIssue issue = RemoteRedmine4_2_10ServiceImpl.mapIssue(data);
+        assertEquals(167994, issue.issueId());
+        assertEquals("Referenced issue", issue.subject());
+        assertTrue("custom fields default to empty", issue.customFields().isEmpty());
+        assertEquals("assignee defaults to empty", "", issue.assigneeName());
+    }
     @Test
     @Ignore
     public void getCommentTest() {
